2ecb200bac0ef7086444ae5d97a1c4864a1af91d
Smith7532 <876674144@qq.com>
2019-02-19 19:03:01 +0800

    api调用未完成

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,7 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
@@ -13,7 +13,28 @@ Page({
   onLoad: function (options) {
     options.id
     this.setData({
-      mid:options.id
+      mid: options.id
+    })
+
+    wx.request({
+      url: '',
+      method: "GET",
+      data: {
+        x: 1, y: 2
+      },
+      header: {
+
+      },
+
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function () {
+
+      },
+      complete: function () {
+
+      }
     })
   },
 
@@ -21,48 +42,51 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  }
+
+  },
+
+  
+
 })
