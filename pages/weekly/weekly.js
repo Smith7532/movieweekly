f4e45b89e2bea3e753053df3fbc52bdd62537080
Smith7532 <876674144@qq.com>
2019-03-22 23:10:28 +0800

    电影mid真实

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -5,14 +5,14 @@ Page({
         comment: "失去才是永恒的",
         imagePath: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550212575397&di=9774b1dcd66c3172c3db376392ed6f6a&imgtype=0&src=http%3A%2F%2Fwww.renderg.com%2Fuploads%2Fallimg%2F171220%2F3_1044436184.jpg",
         isHighlyRecommended: false,
-        id: 77
+        id: 1292722
       },
       {
         name: "这个杀手不太冷",
         comment: "小萝莉与怪蜀黍纯真灿烂的爱情故事",
         imagePath: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1826794556,3178197553&fm=26&gp=0.jpg",
         isHighlyRecommended: false,
-        id: 88
+        id: 1295644
       },
       {
         name: "教父",
