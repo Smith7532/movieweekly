e25df4786a67ae7e14eeed02dcff8e24f147dde5
Smith7532 <44564148+Smith7532@users.noreply.github.com>
2019-06-30 21:25:12 +0800

    Update catch.py
    增加“楚门的世界”
    更改豆瓣apikey

--- catch.py
+++ catch.py
@@ -1,9 +1,9 @@
 import urllib.request
 
-midlist = ['1291841','1292722','1295644']
+midlist = ['1291841','1292722','1295644','1292064']
 
 for mid in midlist:
-    url = "https://api.douban.com/v2/movie/subject/" + mid
+    url = "https://api.douban.com/v2/movie/subject/" + mid + "?apikey=0df993c66c0c636e29ecbb5344252a4a"
     responces = urllib.request.urlopen(url)
     html = responces.read()
     html = html.decode('utf-8')
